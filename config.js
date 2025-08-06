84fc67b9c6a3c47c82b145ed7add6be18680cd18
TØP BØ¥ <nwankwogoodluck156@gmail.com>
2025-08-06 10:20:55 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
+module.exports = {
+  PREFIX: process.env.PREFIX || "IRON-M4N×",
+  EMAIL: process.env.EMAIL || "demo.test.bot15@gmail.com",
+  PASS: process.env.PASS || "dvts eodz jyop dozu",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
+}
