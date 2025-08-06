c0548f0005474ee086782554f09c2242d25c07e5
TØP BØ¥ <nwankwogoodluck156@gmail.com>
2025-08-06 12:23:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
   PREFIX: process.env.PREFIX || "IRON-M4N×",
   EMAIL: process.env.EMAIL || "demo.test.bot15@gmail.com",
   PASS: process.env.PASS || "dvts eodz jyop dozu",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot named 🔥Conspiracy 💫 Made by TopBoy",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
